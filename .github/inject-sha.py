ca755eddf3a51c6c73d3e08f422f746b8e539e4e
Jirka B <j.borovec+github@gmail.com>
2025-07-21 11:18:12 +0200

    REPO_NAME

--- .github/inject-sha.py
+++ .github/inject-sha.py
@@ -1,9 +1,10 @@
 import yaml
 
 THIS_REPO = "https://github.com/JoC0de/pre-commit-prettier"
+ROOT_REPO_NAME = "JoC0de/pre-commit-prettier"
 PRE_COMMIT_CONFIG = ".pre-commit-config.yaml"
 
-def main(sha: str) -> None:
+def main(sha: str, repo: str = ROOT_REPO_NAME) -> None:
     """Inject the SHA into the YAML file."""
     # Load the YAML file
     with open(PRE_COMMIT_CONFIG, "r", encoding="utf-8") as fo:
@@ -15,6 +16,10 @@ def main(sha: str) -> None:
             continue
         print(f"Injecting SHA `{sha}` into repo {repo}")
         config["repos"][i]["rev"] = sha
+        if repo != ROOT_REPO_NAME:
+            repo = repo["repo"].replace(ROOT_REPO_NAME, repo)
+            print(f"Updating repo URL to {repo}")
+            config["repos"][i]["repo"] = repo
 
     # Save the updated config back to the file
     with open(PRE_COMMIT_CONFIG, "w", encoding="utf-8") as fo:
@@ -27,5 +32,6 @@ if __name__ == "__main__":
         sys.exit(1)
 
     sha = sys.argv[1]
-    main(sha)
-    print(f"SHA `{sha}` injected into .pre-commit-config.yaml")
+    repo = sys.argv[2] if len(sys.argv) > 2 else ROOT_REPO_NAME
+    main(sha=sha, repo=repo)
+    print(f"SHA `{sha}` and repo `{repo}` injected into .pre-commit-config.yaml")
